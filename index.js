fc9cdae3e18794bb7adb18b6e8d5fa3c0ded7c7b
emily <limeyb7@gmail.com>
2017-06-11 21:02:34 +0100

    starts refactoring with graphql api

--- index.js
+++ index.js
@@ -2,29 +2,43 @@ const githubHandle = "emilyb7";
 
 /* helper functions */
 
-const getLanguages = arr =>
-  arr.map(({ language, }) => language)
-    .reduce((a, b) => a.concat(!!b && a.indexOf(b) < 0 ? [b] : []), []);
+const getLanguages = repoNodes => repoNodes
+  .map(({ languages, }) => languages.nodes)
+  .reduce((a, b) => a.concat(b), [])
+  .reduce((a, { name, }) => a.concat(a.indexOf(name) > -1 ? [] : [ name, ]), [])
 
-const countStars = arr =>
-  arr.reduce((a, b) => a + b.stargazers_count, 0);
+const countStars = repoNodes => repoNodes
+  .map(({ stargazers, }) => stargazers.nodes)
+  .reduce((a, b) => a + b.length, 0);
 
 const getUsername = ({ login, }) => login;
 
-const success_userDetails = response => ({
+const success_userDetails = ({ data, }) => {
+  console.log("success userDetails");
+
+  console.log(data);
+
+  const firstRepo = data.viewer.repositories.nodes.slice(0).reverse()[1];
+
+  const res = {
     githubHandle: githubHandle,
     link: "https://github.com/" + githubHandle,
-    avatar: response[0].owner.avatar_url,
-    repos: response.length,
-    languages: getLanguages(response),
-    stars: countStars(response),
-    firstRepoName: response[0].name,
-    firstRepoUrl: response[0].html_url,
-    firstRepoCreated: response[0].created_at.substr(0, 10),
-    firstRepoIssues: response[0].open_issues,
-    firstRepoWatchers: response[0].watchers,
-    firstRepoContributors: response[0].contributors_url,
-  });
+    avatar: data.viewer.avatarUrl,
+    repos: data.viewer.repositories.nodes.length,
+    languages: getLanguages(data.viewer.repositories.nodes),
+    stars: countStars(data.viewer.repositories.nodes),
+    firstRepoName: firstRepo.name,
+    firstRepoUrl: firstRepo.url,
+    firstRepoCreated: firstRepo.createdAt,
+    firstRepoIssues: firstRepo.issues.nodes.length,
+    // firstRepoWatchers: response[0].watchers,
+    // firstRepoContributors: response[0].contributors_url,
+  };
+
+  console.log(res);
+
+  return res;
+}
 
 const success_contributorDetails = (args, response) =>
   Object.assign({}, args, { contributors : response.map(getUsername), });
@@ -32,37 +46,49 @@ const success_contributorDetails = (args, response) =>
 
 /* generic request function, can be recycled over and over! */
 
-const request = (url, cb) => {
+const post = (url, query, cb) => {
 
   const init = {
-    method: 'GET',
-    mode: 'cors',
-    cache: 'default',
+    method: 'POST',
+    body: JSON.stringify(query),
+    headers: new Headers({
+      'Authorization': token,
+    }),
   }
 
   fetch(url, init).then((response) => {
     return response.json()
   }).then(cb)
+  .catch((err) => console.log(err))
+
 }
 
-const updateDOM = obj => {
-  document.getElementById("github-user-handle").textContent = githubHandle;
-  document.getElementById("github-user-link").href = "https://github.com/" + githubHandle;
-  document.getElementById("github-user-avatar").src = obj.avatar;
-  document.getElementById("github-user-repos").textContent = obj.repos;
-  document.getElementById("github-repos-languages").textContent = obj.languages.join(", ");
-  document.getElementById("github-repos-stars").textContent = obj.stars;
-  document.getElementById("github-repo-name").textContent = obj.name;
-  document.getElementById("github-repo-link").href = obj.url;
-  document.getElementById("github-repo-created").textContent = obj.firstRepoCreated;
-  document.getElementById("github-repo-open-issues").textContent = obj.firstRepoIssues;
-  document.getElementById("github-repo-watchers").textContent = obj.firstRepoWatchers;
-  document.getElementById("github-repo-contributors").textContent = obj.contributors.join(", ");
+// const updateDOM = obj => {
+//   document.getElementById("github-user-handle").textContent = githubHandle;
+//   document.getElementById("github-user-link").href = "https://github.com/" + githubHandle;
+//   document.getElementById("github-user-avatar").src = obj.avatar;
+//   document.getElementById("github-user-repos").textContent = obj.repos;
+//   document.getElementById("github-repos-languages").textContent = obj.languages.join(", ");
+//   document.getElementById("github-repos-stars").textContent = obj.stars;
+//   document.getElementById("github-repo-name").textContent = obj.name;
+//   document.getElementById("github-repo-link").href = obj.url;
+//   document.getElementById("github-repo-created").textContent = obj.firstRepoCreated;
+//   document.getElementById("github-repo-open-issues").textContent = obj.firstRepoIssues;
+//   document.getElementById("github-repo-watchers").textContent = obj.firstRepoWatchers;
+//   document.getElementById("github-repo-contributors").textContent = obj.contributors.join(", ");
+// }
+
+const query = {
+  query: "query { viewer { login, avatarUrl, repositories(last: 50) { nodes { name, url, createdAt, languages(first: 10) { nodes { name }}, stargazers(first: 100) { nodes { name }}, issues(first:100) { nodes { url, }} }} } }",
 }
 
-request("https://api.github.com/users/" + githubHandle + "/repos", (response) => {
+post("https://api.github.com/graphql", query, (response) => {
+  console.log(response);
+  console.log("hello");
   const userDetails = success_userDetails(response)
-  request(userDetails.firstRepoContributors, (contribResponse) => {
-    updateDOM(success_contributorDetails(userDetails, contribResponse))
-  })
+  console.log(userDetails);
+  // // request(userDetails.firstRepoContributors, (contribResponse) => {
+  //   console.log(contribResponse);
+    // updateDOM(success_contributorDetails(userDetails, contribResponse))
+  // })
 })
